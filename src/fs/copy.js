d478c4aa6735b05925c19ec197b51fe002d34a41
Yevhenii <sharovskiyevgeniy@gmail.com>
2022-12-04 20:26:31 +0200

    feat(fs): add solve for copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,25 @@
+import { readdir, mkdir, copyFile } from 'node:fs/promises';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    await mkdir(__dirname + '/files_copy');
+
+    const files = await readdir(__dirname + '/files');
+    for (const file of files) {
+      await copyFile(
+        `${__dirname}/files/${file}`,
+        `${__dirname}/files_copy/${file}`
+      );
+    }
+  } catch (err) {
+    if (err.syscall === 'mkdir' || err.syscall === 'scandir') {
+      throw new Error('FS operation failed');
+    }
+  }
 };
 
 await copy();
