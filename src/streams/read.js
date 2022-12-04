c26661bb215f8de86635d08968742ac82adeea9e
Yevhenii <sharovskiyevgeniy@gmail.com>
2022-12-04 23:52:31 +0200

    feat(streams): add solve for read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import { open } from 'node:fs/promises';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const stream = (
+      await open(__dirname + '/files/fileToRead.txt')
+    ).createReadStream({ encoding: 'utf8' });
+
+    stream.pipe(process.stdout);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 await read();
