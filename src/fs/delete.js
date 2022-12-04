9f272c0f5067e37b48ed9d0d43f37dcfe0430d7a
Yevhenii <sharovskiyevgeniy@gmail.com>
2022-12-04 21:32:41 +0200

    feat(fs): add solve for delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,15 @@
+import { rm } from 'node:fs/promises';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    await rm(__dirname + '/files/fileToRemove.txt');
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await remove();
